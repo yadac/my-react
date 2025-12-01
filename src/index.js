24513b2a000aca21962ccd3ba394e76a13281425
Yosuke Adachi <doruji1982@gmail.com>
2025-12-01 23:03:54 +0900

    feat: アプリの再描画

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+const interval = 1000;
+setInterval(() => {
+  root.render(
+    <p>現在時刻：{(new Date().toLocaleString())}</p>
+  )
+}, interval);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
