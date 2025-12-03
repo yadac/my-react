fb649d7b5bbfb198ceabc22c26406d1539038917
Yosuke Adachi <doruji1982@gmail.com>
2025-12-03 22:44:21 +0900

    feat:props

---
+++ src/components/MyHello.js
@@ -0,0 +1,7 @@
+export default function MyHello(props) {
+    return (
+        <div>
+            こんにちは！{props.myName}さん
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MyHello from './components/MyHello'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-const interval = 1000;
-setInterval(() => {
-  root.render(
-    <p>現在時刻：{(new Date().toLocaleString())}</p>
-  )
-}, interval);
+root.render(
+  <React.StrictMode>
+    <MyHello myName="yosuke" />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
