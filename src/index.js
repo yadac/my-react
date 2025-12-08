8bc0c7de4cc76e16ba3e1faa90716977b24cdca7
Yosuke Adachi <doruji1982@gmail.com>
2025-12-08 23:16:33 +0900

    feat:ファイル入力ボックスの非制御コンポーネント

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import StateFormUC from './components/StateFormUC';
+import FormFile from './components/FormFile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StateFormUC />
+    <FormFile />
   </React.StrictMode>
 );
 
