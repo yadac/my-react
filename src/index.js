f455b67e4a999d4c1f62055bc56f45842f13c424
Yosuke Adachi <doruji1982@gmail.com>
2025-12-13 21:50:54 +0900

    入れ子になったStateの更新

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import FormFile from './components/FormFile';
+import StateNest from './components/StateNest';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <FormFile />
+    <StateNest />
   </React.StrictMode>
 );
 
