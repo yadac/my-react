b936d81b6a573e30e171c2f968f13906d4c0d746
Yosuke Adachi <doruji1982@gmail.com>
2026-01-02 23:36:33 +0900

    feat: tanstack-queryでFetch

---
+++ src/components/QueryBasic.js
@@ -0,0 +1,46 @@
+import { useQuery } from '@tanstack/react-query';
+import { Weather_API_KEY } from '../config/env';
+
+const city = 'Sapporo';
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Weather_API_KEY}`;
+const sleep = delay => new Promise(resolve => setTimeout(resolve, delay))
+
+const fetchWeather = async () => {
+    await sleep(2_000);
+    const res = await fetch(url);
+    if (res.ok) {
+        return res.json();
+    }
+    throw new Error(res.statusText);
+}
+
+export default function QueryBasic() {
+
+    const { data, isLoading, isError, error, dataUpdatedAt } = useQuery({
+        queryKey: ['weather'],
+        queryFn: fetchWeather,
+        refetchInterval: 60_000
+    });
+
+    if (isLoading) {
+        return (<p>Loading...</p>);
+    }
+    if (isError) {
+        return (<p>Error:{error}</p>);
+    }
+    return (
+        <div className='h-screen flex flex-col items-center justify-center'>
+            <figure className="flex flex-col items-center">
+                <img
+                    src={`https://openweathermap.org/img/wn/${data?.weather?.[0]?.icon}.png`}
+                    alt={data?.weather?.[0]?.main}
+                />
+                <figcaption>{data?.weather?.[0]?.description}</figcaption>
+            </figure>
+            <div className="text-sm text-gray-500">
+                {new Date(dataUpdatedAt).toLocaleString()}
+            </div>
+        </div>
+
+    );
+}
--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import theme from './theme';
-import QueryPre from './components/QueryPre';
+import QueryBasic from './components/QueryBasic';
 
+const cli = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <QueryPre />
+      <QueryClientProvider client={cli}>
+        <QueryBasic />
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
