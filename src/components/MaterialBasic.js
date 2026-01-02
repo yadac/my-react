52d226c41dff27c7668468c4d7b710d0a29ce40c
Yosuke Adachi <doruji1982@gmail.com>
2026-01-02 20:26:02 +0900

    feat: MUIの基本

---
+++ src/components/MaterialBasic.js
@@ -0,0 +1,11 @@
+import { Button } from '@mui/material';
+
+export default function MaterialBasic() {
+    return (
+        <>
+            <Button variant='text'>Text</Button>
+            <Button variant='contained'>Contained</Button>
+            <Button variant='outlined'>Outlined</Button>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import ErrorEventRoot from './components/ErrorEventRoot';
+import MaterialBasic from './components/MaterialBasic';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ErrorEventRoot />
+    <MaterialBasic />
   </React.StrictMode>
 );
 
